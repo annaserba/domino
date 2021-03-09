d6e0fffbf08bb9a04f132eb6566f0a0c675373df
Anna.Serba <annserba94@yandex.ru>
2021-03-09 20:58:50 +0300

    format styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,45 @@
-const { src, dest, watch } = require('gulp');
+const {
+  src,
+  dest,
+  watch
+} = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
-  
-  function javascript(cb) {
-    return src('src/**/*.js')
+
+function javascript(cb) {
+  return src('src/**/*.js')
     .pipe(babel())
     .pipe(dest('dist/'))
     .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
+    .pipe(rename({
+      extname: '.min.js'
+    }))
     .pipe(dest('./dist/'));
-  }
-  
-  function css(cb) {
-   return src('src/**/*.scss') 
+}
+
+function css(cb) {
+  return src('src/**/*.scss')
     .pipe(sass())
     .pipe(dest('./dist/'))
-  }
-exports.default = function() {
+}
+exports.default = function () {
   css();
   javascript();
   watch('src/**/*.scss', css);
   watch('src/**/*.js', javascript);
   src([
-    './node_modules/angular-material/angular-material.css',
-  ])        
-  .pipe(dest('./dist/lib/css/'))
+      './node_modules/angular-material/angular-material.css',
+    ])
+    .pipe(dest('./dist/lib/css/'))
   return src([
-    './node_modules/angular/angular.js',
-    './node_modules/angular-animate/angular-animate.js',
-    './node_modules/angular-aria/angular-aria.js',
-    './node_modules/angular-messages/angular-messages.js',
-    './node_modules/angular-material/angular-material.js',
-    './node_modules/fabric/dist/fabric.js'
-  ])        
-  .pipe(dest('./dist/lib/js/'));
+      './node_modules/angular/angular.js',
+      './node_modules/angular-animate/angular-animate.js',
+      './node_modules/angular-aria/angular-aria.js',
+      './node_modules/angular-messages/angular-messages.js',
+      './node_modules/angular-material/angular-material.js',
+      './node_modules/fabric/dist/fabric.js'
+    ])
+    .pipe(dest('./dist/lib/js/'));
 }
